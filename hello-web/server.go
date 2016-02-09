78ec78742aaa6837d3bb250787543e08e4de1770
Brian Shef <bshef@tradestation.com>
2016-02-09 11:44:50 -0600

    Refactor for improved scalabiltiy and maintainability
    Adding API routes is as easy as adding the path and the function to the mapping, and defining the handler function.

--- hello-web/server.go
+++ hello-web/server.go
@@ -6,27 +6,54 @@ import (
 	"strconv"
 )
 
-// Variables
+// CONSTANT Variables
 const port int = 8000
-var portString string = ":" + strconv.Itoa(port)
+const serverHeader string = "Hello Go Web Server"
 
-func setServerHeader(w http.ResponseWriter) {
-    w.Header().Set("Server", "Hello Go Web Server");
-}
+//
+//	API Handler Functions
+//
 
+//	Responds with "Hello, world!"
 func hello(w http.ResponseWriter, req *http.Request) {
-    setServerHeader(w)
+	setServerHeader(w)
 	io.WriteString(w, "Hello, world!")
 }
 
+//	Responds with 200 OK
 func health(w http.ResponseWriter, req *http.Request) {
-    setServerHeader(w)
-    w.WriteHeader(200);
+	setServerHeader(w)
+	w.WriteHeader(200)
 }
 
+//
+//	Server functions
+//
+
+//	Sets the response header
+func setServerHeader(w http.ResponseWriter) {
+	w.Header().Set("Server", serverHeader)
+}
+
+//	Maps API handler functions to the API path, as specified in a mapping
+func mapAPIfunctions(mux *http.ServeMux, apiMap map[string]func(http.ResponseWriter, *http.Request)) {
+	for path, apiFunction := range apiMap {
+		mux.HandleFunc(path, apiFunction)
+	}
+}
+
+//	Logic that occurs when server starts
+func startServer(port int, mux *http.ServeMux, apiMap map[string]func(http.ResponseWriter, *http.Request)) {
+	mapAPIfunctions(mux, apiMap)
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+}
+
+//
+//	Server main entry point
+//
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-    mux.HandleFunc("/health", health)
-	http.ListenAndServe(portString, nil)
+	startServer(port, http.NewServeMux(), map[string]func(http.ResponseWriter, *http.Request){
+		"/":       hello,
+		"/health": health,
+	})
 }
